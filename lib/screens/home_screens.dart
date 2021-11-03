a028171445fc098ed3122f3dcabcda762ea50e4b
Zulfadlifauzi <fauzi_zulfadli@yahoo.com>
2021-11-03 18:18:03 +0800

    add button reset and amount of people_Day 1.5

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -17,6 +17,7 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Container(
           color: color.AppColor.kPrimaryColor,
           width: double.infinity,
+          height: 862.0,
           child: Column(
             children: [
               Center(
@@ -73,6 +74,225 @@ class _HomeScreenState extends State<HomeScreen> {
                               style: textFieldStyle,
                               textAlign: TextAlign.end,
                             ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Text(
+                              'Select Tip %',
+                              style: textStyle,
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        primary:
+                                            color.AppColor.kElevetedButtonColor,
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 20)),
+                                    onPressed: () {},
+                                    child: Text('5%', style: textButtonStyle),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        primary:
+                                            color.AppColor.kElevetedButtonColor,
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 20)),
+                                    onPressed: () {},
+                                    child: Text('10%', style: textButtonStyle),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        primary:
+                                            color.AppColor.kElevetedButtonColor,
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 20)),
+                                    onPressed: () {},
+                                    child: Text('15%', style: textButtonStyle),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        primary:
+                                            color.AppColor.kElevetedButtonColor,
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 20)),
+                                    onPressed: () {},
+                                    child: Text('20%', style: textButtonStyle),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                        primary:
+                                            color.AppColor.kElevetedButtonColor,
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 20)),
+                                    onPressed: () {},
+                                    child: Text('25%', style: textButtonStyle),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                Expanded(
+                                  child: TextField(
+                                    keyboardType:
+                                        const TextInputType.numberWithOptions(),
+                                    decoration: const InputDecoration(
+                                      filled: true,
+                                      fillColor: color.AppColor.kTextFieldColor,
+                                      border: InputBorder.none,
+                                      hintText: 'Custom',
+                                    ),
+                                    style: textFieldStyle,
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Number of People',
+                                  style: subTitleStyle,
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                TextField(
+                                  keyboardType:
+                                      const TextInputType.numberWithOptions(),
+                                  decoration: const InputDecoration(
+                                      filled: true,
+                                      fillColor: color.AppColor.kTextFieldColor,
+                                      border: InputBorder.none,
+                                      hintText: 'eg 5',
+                                      prefixIcon: Icon(Icons.person)),
+                                  style: textFieldStyle,
+                                  textAlign: TextAlign.end,
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              padding:
+                                  const EdgeInsets.only(left: 20, right: 20),
+                              width: double.infinity,
+                              height: 175,
+                              decoration: BoxDecoration(
+                                  color: color.AppColor.kElevetedButtonColor,
+                                  borderRadius: BorderRadius.circular(20)),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            'Tip Amount',
+                                            style: color.textTipAmountStyle,
+                                          ),
+                                          const SizedBox(
+                                            height: 5,
+                                          ),
+                                          Text(
+                                            '/ person',
+                                            style: color.subTextTipAmountStyle,
+                                          )
+                                        ],
+                                      ),
+                                      Text(
+                                        '\$0.0',
+                                        style: color.subTextTipAmountStyle1,
+                                      )
+                                    ],
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            'Total',
+                                            style: color.textTipAmountStyle,
+                                          ),
+                                          const SizedBox(
+                                            height: 5,
+                                          ),
+                                          Text(
+                                            '/ person',
+                                            style: color.subTextTipAmountStyle,
+                                          )
+                                        ],
+                                      ),
+                                      Text(
+                                        '\$0.0',
+                                        style: color.subTextTipAmountStyle1,
+                                      )
+                                    ],
+                                  ),
+                                  const SizedBox(height: 10),
+                                  ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 120, vertical: 10),
+                                          primary: color.AppColor.kSubTipStyle),
+                                      onPressed: () {},
+                                      child: Text(
+                                        'RESET',
+                                        style: color.buttonTipStyle,
+                                      ))
+                                ],
+                              ),
+                            )
                           ],
                         )),
                   ],
--- lib/styles/styles.dart
+++ lib/styles/styles.dart
@@ -4,12 +4,12 @@ import 'package:tipcalculator/styles/styles.dart' as color;
 
 class AppColor {
   static const kPrimaryColor = Color(0xFFb0e0e6);
-  static const kSecondaryColor = Color(0xFFFE9901);
-  static const kContentColorLightTheme = Color(0xFF1D1D35);
+  static const kElevetedButtonColor = Color(0xFF00474B);
+  static const kSubTipStyle = Color(0xFF29c0AD);
   static const kContentColorDarkTheme = Color(0x00000000);
   static const kWarninngColor = Color(0xFFF3BB1C);
   static const kErrorColor = Color(0xFFF03738);
-  static const kTextColor = Color(0xFF58804D);
+  static const kTextColor = Color(0xFF5B777B);
   static const kBackgroundColor = Color(0xFFFFFBF5);
   static const kTextFieldColor = Color(0xFFF3F8FB);
 }
@@ -26,10 +26,64 @@ TextStyle get titleStyle {
 TextStyle get subTitleStyle {
   return GoogleFonts.lato(
       textStyle: (const TextStyle(
-          fontSize: 20, color: Colors.grey, fontWeight: FontWeight.bold)));
+          fontSize: 20,
+          color: color.AppColor.kTextColor,
+          fontWeight: FontWeight.bold)));
 }
 
 TextStyle get textFieldStyle {
   return GoogleFonts.lato(
       textStyle: (const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)));
+}
+
+TextStyle get textStyle {
+  return GoogleFonts.lato(
+      textStyle: (const TextStyle(
+          fontSize: 15,
+          fontWeight: FontWeight.bold,
+          color: color.AppColor.kTextColor)));
+}
+
+TextStyle get textButtonStyle {
+  return GoogleFonts.lato(
+      textStyle: (const TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+  )));
+}
+
+TextStyle get textTipAmountStyle {
+  return GoogleFonts.lato(
+      textStyle: (const TextStyle(
+    fontSize: 20,
+    color: Colors.white,
+    fontWeight: FontWeight.bold,
+  )));
+}
+
+TextStyle get subTextTipAmountStyle {
+  return GoogleFonts.lato(
+      textStyle: (const TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+    color: color.AppColor.kTextColor,
+  )));
+}
+
+TextStyle get subTextTipAmountStyle1 {
+  return GoogleFonts.lato(
+      textStyle: (const TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: color.AppColor.kSubTipStyle,
+  )));
+}
+
+TextStyle get buttonTipStyle {
+  return GoogleFonts.lato(
+      textStyle: (const TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: color.AppColor.kElevetedButtonColor,
+  )));
 }
